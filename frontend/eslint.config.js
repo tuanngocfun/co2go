fdcdf0c4b9cecd2fe383cd0239bb2c39dee263f8
tuanngocfun <tuanngoccs50@gmail.com>
2025-11-11 11:48:14 +0700

    feat: initialize frontend with Vite and React setup
    - Added package.json for frontend with dependencies and scripts for development, build, linting, and preview.
    - Included Vite configuration for React with a proxy setup for API requests.
    - Added SVG assets for Vite and React logos in the public and assets directories respectively.

---
+++ frontend/eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from '@eslint/js'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  ...js.configs.recommended,
+  ...reactHooks.configs['recommended-latest'],
+  ...reactRefresh.configs.vite,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
